22d3a8dd48a99e6489df829d8273100ed99cd646
canyolal <canyolal16@gmail.com>
2021-12-27 18:37:38 +0300

    remove async/await in service

--- src/services/assignment.js
+++ src/services/assignment.js
@@ -1,14 +1,14 @@
 const records = require('../models/records')
 
 // eslint-disable-next-line no-unused-vars
-const getAllDocs = async (body) => {
+const getAllDocs = (body) => {
 
   //ISODate conversion to compare mongo's format
-  // const sDate = new Date(body.startDate)
-  // const eDate = new Date(body.endDate)
+  const sDate = new Date(body.startDate)
+  const eDate = new Date(body.endDate)
 
   //Filter date while matching and use projection to fetch only useful fields
-  return await records.find({ },{
+  return records.find({ createdAt : {$gte: sDate.toISOString(), $lt: eDate.toISOString()}},{
     counts: true,
     createdAt: true,
     key: true,
