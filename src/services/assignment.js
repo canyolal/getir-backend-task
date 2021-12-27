07c06ba97a4f8ef8b6edcae03c7b695596af405c
canyolal <canyolal16@gmail.com>
2021-12-27 18:33:38 +0300

    deployment fixes

--- src/services/assignment.js
+++ src/services/assignment.js
@@ -1,13 +1,14 @@
 const records = require('../models/records')
 
+// eslint-disable-next-line no-unused-vars
 const getAllDocs = async (body) => {
 
   //ISODate conversion to compare mongo's format
-  const sDate = new Date(body.startDate)
-  const eDate = new Date(body.endDate)
+  // const sDate = new Date(body.startDate)
+  // const eDate = new Date(body.endDate)
 
   //Filter date while matching and use projection to fetch only useful fields
-  return await records.find({ createdAt : {$gte: sDate.toISOString(), $lt: eDate.toISOString()}},{
+  return await records.find({ },{
     counts: true,
     createdAt: true,
     key: true,
